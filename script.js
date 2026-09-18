c411def477c4ca5eedba87fc7dc70b2ace204d62
Mtnepro <2mtneatelier@gmail.com>
2026-09-18 09:26:37 +0200

    Initialize Supabase client in script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const SUPABASE_URL = "https://cbzslusotuxjep...supabase.co";
+const SUPABASE_KEY = "sb_publishable-...";
 
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+console.log("Supabase est connecté !");
